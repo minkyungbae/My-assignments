004be423e9040ea11bb08c009ce9cd1d46c68d97
minkyungbae <bmk0703@naver.com>
2025-01-20 12:45:38 +0900

    Fix code, add AUTH_USER_MODEL

--- Chapter3_Assignment_250123/my_pjt/my_pjt/settings.py
+++ Chapter3_Assignment_250123/my_pjt/my_pjt/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+AUTH_USER_MODEL = 'users_APP.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
