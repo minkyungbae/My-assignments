dc565e438af0c61408374e374e7137db65c9a01b
minkyungbae <bmk0703@naver.com>
2025-01-21 00:32:49 +0900

    Fix code, add Article, comment model

--- Chapter3_Assignment_250123/post/models.py
+++ Chapter3_Assignment_250123/post/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# 글 작성하는 모델
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+# 댓글 기능하는 모델
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name="comments")
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
