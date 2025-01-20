d1ee828cd8af9e0606ccdd42f323fecc312e81ec
minkyungbae <bmk0703@naver.com>
2025-01-20 17:30:59 +0900

    Fix code, add CostomUser model

--- Chapter3_Assignment_250123/user/models.py
+++ Chapter3_Assignment_250123/user/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
