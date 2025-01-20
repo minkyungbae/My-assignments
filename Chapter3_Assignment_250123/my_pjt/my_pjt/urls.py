7a0539feded51c798f699c03cfbb3bf775ed6bda
minkyungbae <bmk0703@naver.com>
2025-01-20 13:09:06 +0900

    Fix code, add users_APP, accounts_APP url

--- Chapter3_Assignment_250123/my_pjt/my_pjt/urls.py
+++ Chapter3_Assignment_250123/my_pjt/my_pjt/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users_APP/', include('users_APP.urls')),
+    path('accounts_APP/', include('accounts_APP.urls')),
 
 ]
