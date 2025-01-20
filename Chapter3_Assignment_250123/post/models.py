c447c88358781ee75b14ad557763e99f3cd3ded2
minkyungbae <bmk0703@naver.com>
2025-01-21 00:46:09 +0900

    Fix code, add author

--- Chapter3_Assignment_250123/post/models.py
+++ Chapter3_Assignment_250123/post/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # 글 작성하는 모델
 class Article(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
 
 # 댓글 기능하는 모델
 class Comment(models.Model):
