054781dd391e12c1c066d0b56829ab9acbf848fd
minkyungbae <bmk0703@naver.com>
2025-01-21 00:31:58 +0900

    Fix code, def post_list

--- Chapter3_Assignment_250123/post/views.py
+++ Chapter3_Assignment_250123/post/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Article
 
 
 # index 보기
 def index(request):
     return render(request, "post/index.html")
+
+
+# post list 보기
+def post_list(request):
+    post_list = Article.objects.all()
+    context = {"post_list":post_list}
+    return render(request, "post/post_list.html", context)
 
 
